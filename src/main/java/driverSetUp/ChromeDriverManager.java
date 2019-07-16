d8445dfafc9a6b4e42559efd843f1dd4f582fd05
NadyaEvst <evstick@gmail.com>
2019-07-16 13:27:50 +0200

    Create ChromeDriverManager.java

---
+++ src/main/java/driverSetUp/ChromeDriverManager.java
@@ -0,0 +1,20 @@
+package driverSetUp;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.thucydides.core.webdriver.DriverSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriverManager implements DriverSource {
+
+    @Override
+    public WebDriver newDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
+    @Override
+    public boolean takesScreenshots() {
+        return true;
+    }
+}
